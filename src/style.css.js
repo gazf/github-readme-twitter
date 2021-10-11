5b6a24b8a0b448c7ce888d6c83e3ef8a46352b97
gazf <gazfff@gmail.com>
2021-10-11 13:33:31 +0900

    add dark mode option

--- src/style.css.js
+++ src/style.css.js
@@ -11,9 +11,15 @@ export default `
   stroke-opacity: 1;
   fill: #fffefe;
 }
+.card-bg.darkmode{
+  fill: #000;
+}
 .header{
   font: 600 18px 'Segoe UI', Ubuntu, Sans-Serif;
   fill: #2f80ed;
+}
+.header.darkmode{
+  fill: #fff;
 }
 .tw-wrapper{
   overflow: hidden;
@@ -27,13 +33,23 @@ export default `
   font-weight: bold;
   font-size: 13px;
 }
+.tw-name.darkmode{
+  color: rgb(255,255,255);
+}
 .tw-screen-name{
   color: rgb(101, 119, 134);
   font-size: 12px;
   margin-left: .5em;
 }
+.tw-screen-name.darkmode{
+  color: rgb(172, 204, 231);
+}
 .tw-text{
   color: rgb(20, 23, 26);
+  font-size: 12px;
+}
+.tw-text.darkmode{
+  color: rgb(227, 227, 227);
   font-size: 12px;
 }
 `;
